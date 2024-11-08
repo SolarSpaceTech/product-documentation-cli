d591139640bbe5156ac6125f85d4d8b1bc95c489
Maksim Voropay <maksim.voropay@mvideo.ru>
2024-11-08 11:38:54 +0300

    updated create util for mac

--- util-builder/create-mac.sh
+++ util-builder/create-mac.sh
@@ -5,6 +5,7 @@ OS="darwin"
 EXT="tar.gz"
 BUILD_DIR="build"
 UTIL_FILE="index.js"
+PLUGINS_DIR="plugins"
 
 ESBUILD_CONFIG_FILE="esbuild.js"
 echo "Сборка CLI утилиты..."
@@ -31,8 +32,11 @@ tar -xzf $FILE_NAME -C .
 
 NODE_DIR="node-v$VERSION-$OS-$ARCH"
 
-# Копируем Node.js
+# Копируем Node.js и плагины
 mv $NODE_DIR/bin/node $BUILD_DIR
+if [ -d $PLUGINS_DIR ]; then
+  cp -R $PLUGINS_DIR $BUILD_DIR
+fi
 
 # Создаем архив с содержимым сборки
 ARCHIVE_NAME="node_bundle.$EXT"
@@ -45,22 +49,32 @@ cat << 'EOF' > run.sh
 #!/bin/bash
 
 CURRENT_DIR=$(dirname "$(readlink -f "$0")")
-TEMP_DIR="$temp"
-BIN_DIR="bin"
+BIN_DIR="$CURRENT_DIR/bin"
 PLUGINS_DIR="plugins"
 export PD_CONTENT_DIR="../content"
 
 if [ ! -d $BIN_DIR ]; then
     echo "Директория bin не найдена. Создаю bin и распаковываю файлы..."
-    mkdir -p "$CURRENT_DIR$TEMP_DIR" "$CURRENT_DIR$BIN_DIR" "$CURRENT_DIR$PLUGINS_DIR"
-    tail -n +21 "$0" | tar -x -C "$CURRENT_DIR$TEMP_DIR"
-    mv "$CURRENT_DIR$TEMP_DIR/$PLUGINS_DIR" "$CURRENT_DIR$PLUGINS_DIR"
-    mv "$CURRENT_DIR$TEMP_DIR" "$CURRENT_DIR$BIN_DIR"
+    mkdir -p "$BIN_DIR" "$CURRENT_DIR/$PLUGINS_DIR"
+    tail -n +31 "$0" | tar -x -C $BIN_DIR
+    mv "$BIN_DIR/$PLUGINS_DIR" "$CURRENT_DIR"
+    mkdir "$BIN_DIR/$PLUGINS_DIR"
     echo "Файлы успешно распакованы"
 fi
 
+cd $CURRENT_DIR
+
+if [ -d $CURRENT_DIR/$PLUGINS_DIR ]; then
+    echo "Инициализация плагинов"
+    for script in $CURRENT_DIR/$PLUGINS_DIR/*; do
+        if [ -f $script ]; then
+            bash $script
+        fi
+    done
+fi
+
 echo "Запуск программы"
-cd "$CURRENT_DIR$BIN_DIR"
+cd "$BIN_DIR"
 exec ./node ./index.js
 EOF
 
